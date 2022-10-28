740660d9c82c8451444c52b7bad57caade101e5f
Mikael da Silva <84426655+MikaeldaSilvaS@users.noreply.github.com>
2022-10-27 23:49:52 -0300

    corpus

--- Corpus.py
+++ Corpus.py
@@ -2,21 +2,25 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 import spacy
+import numpy as np
 
 def createList(url):
   nlp = spacy.load('en_core_web_sm')
-  html = urlopen(url).read()
-  soup = BeautifulSoup(html, 'html.parser')
-  [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title', 'footer', 'img', 'input', 'header', 'noscript','meta'])]
+  html = urlopen(url)
+  soup = BeautifulSoup(html, 'lxml')
+  [s.extract() for s in soup(['style', 'script', 'document', 'head', 'title', 'footer', 'img', 'input', 'header', 'noscript','meta', 'a', 'nav', 'span', 'h1', 'h2', 'h3', 'time', 'persistent-banner', 'option', 'svg', 'button', 'ul', 'link'])]
   texto = soup.getText()
+
   linhas = (linha.strip() for linha in texto.splitlines())
   separaTexto = (frase.strip() for linha in linhas for frase in linha.split('  '))
   texto = '\n'.join(st for st in separaTexto if st)
-  texto = re.sub('\n', ' ', texto)
+  texto = re.sub('\n', '  ', texto)
+
   text = nlp(texto)
   sentencas = list(text.sents)
 
   return sentencas
+
 
 lista1 = createList('https://www.ibm.com/cloud/learn/natural-language-processing')
 lista2 = createList('https://www.tableau.com/learn/articles/natural-language-processing-examples')
